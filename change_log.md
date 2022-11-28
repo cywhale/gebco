b872090b57c09ba42a09ac7cbc1f4ae07214f75b
cywhale <cyrain@gmail.com>
2022-11-28 14:49:00 +0800

    add requirements.txt

--- change_log.md
+++ change_log.md
@@ -1,6 +1,6 @@
 ## ver 0.1.1 port on server 20221125
 
-## ver 0.1.2 Column-output(default)/dataframe-output(row), point/line in mode(query string)
+## ver 0.1.2 Column(default)/row-output(row), point/line in mode(query string)
 
 #### fix last end-point not in output
 
--- simu.sh
+++ simu.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-# most-performant, add worker, --threads, --worker-connections seems no extra benefits
-gunicorn read_gebco01:app -w 4 -k uvicorn.workers.UvicornWorker --reload
-# maximum no-error limit (if -p 3, always throw error)
-autocannon -c 100 -d 4 -p 2 -w 3 http://localhost:8000/
-# delete gunicorn proc
-ps -ef | grep 'gunicorn' | grep -v grep | awk '{print $2}' | xargs -r kill -9
+#### most-performant, add worker, --threads, --worker-connections seems no extra benefits
+# gunicorn read_gebco01:app -w 4 -k uvicorn.workers.UvicornWorker --reload
+#### maximum no-error limit (if -p 3, always throw error)
+# autocannon -c 100 -d 4 -p 2 -w 3 http://localhost:8000/
+#### delete gunicorn proc
+# ps -ef | grep 'gunicorn' | grep -v grep | awk '{print $2}' | xargs -r kill -9
 
 # https
+source "$HOME/python/py38/bin/activate"
+cd "$HOME/python/gebco"
 gunicorn read_gebco01:app -w 4 -k uvicorn.workers.UvicornWorker -b 127.0.0.1:8013 --keyfile conf/privkey.pem --certfile conf/fullchain.pem --reload
